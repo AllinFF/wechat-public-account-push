3853ef66227820080d5dca7a885ef8bc1160d294
AllinFF <113572912+AllinFF@users.noreply.github.com>
2022-09-15 14:28:11 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oFpn16JoDgjsRnBwCxu6H13LRRSI",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "-mEpW_J37KTx1rkdlPDSOfOy4pArRo8jW5WbaeyJ77_k",
+      useTemplateId: "mEpW_J37KTx1rkdlPDSOfOy4pArRo8jW5WbaeyJ77_k",
       // 所在省份
       province: "浙江",
       // 所在城市
